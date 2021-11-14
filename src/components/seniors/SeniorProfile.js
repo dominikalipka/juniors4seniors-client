a73ad73ebc16778fd93e73e29c7009ac2f3b7327
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-14 17:03:35 +0100

    Added option to assign user to senior after clicking the button

--- src/components/seniors/SeniorProfile.js
+++ src/components/seniors/SeniorProfile.js
@@ -5,73 +5,91 @@ import { Link } from "react-router-dom";
 import AddNeed from "../needs/AddNeed";
 
 class SeniorProfile extends React.Component {
-    state={};
+  state = {};
 
-    getSenior = () => {
-        const {params} = this.props.match;
+  getSenior = () => {
+    const { params } = this.props.match;
 
-        axios
-          .get(`http://localhost:5005/api/seniors/${params.id}`)
-          .then((seniorFromAPI) => {
-            const theSenior = seniorFromAPI.data;
-            this.setState(theSenior);
-            console.log(theSenior)
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-    }
+    axios
+      .get(`http://localhost:5005/api/seniors/${params.id}`)
+      .then((seniorFromAPI) => {
+        const theSenior = seniorFromAPI.data;
+        this.setState(theSenior);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    componentDidMount() {
-        this.getSenior()
-    }
+  assignUserToSenior = () => {
+    console.log(this.props.currentUser._id);
+    const helper = this.props.currentUser._id
+    const { params } = this.props.match;
 
-    render() {
-        return (
+    axios
+      .put(`http://localhost:5005/api/seniors/${params.id}`, { helper })
+      .then(() => this.getSenior())
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount() {
+    this.getSenior();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="senior-profile-container">
+          <div className="center-div">
+            <img src={this.state.image} alt="" />
+            <button
+              onClick={() => this.assignUserToSenior()}
+              className="square-button"
+            >
+              I want to help
+            </button>
+          </div>
           <div>
-            <div className="senior-profile-container">
-              <div>
-                <img src={this.state.image} alt="" />
-              </div>
-              <div>
-                <h2>{this.state.name}</h2>
+            <h2>{this.state.name}</h2>
 
-                <h4>Location: {this.state.location}</h4>
-                <h4>Contact: {this.state.contact}</h4>
-                {this.state.needsList && this.state.needsList.length > 0 && (
-                  <h4>Needs:</h4>
-                )}
-                {this.state.needsList &&
-                  this.state.needsList.map((need, index) => {
-                    return (
-                      <Link
-                        style={{ textDecoration: "none" }}
-                        to={`/seniors/${this.state._id}/needs/${need._id}`}
-                      >
-                        <div className="senior-profile-need" key={index}>
-                          {need.title}
-                        </div>
-                      </Link>
-                    );
-                  })}
-                <div>
-                  <AddNeed theSenior={this.state} getSenior={this.getSenior} />{" "}
-                </div>
-              </div>
-            </div>
-
-            <div className='increase-bottom-margin'>
-              <Link
-                style={{ textDecoration: "none" }}
-                className="form-button"
-                to={"/seniors"}
-              >
-                Back to Seniors
-              </Link>
+            <h4>Location: {this.state.location}</h4>
+            <h4>Contact: {this.state.contact}</h4>
+            {this.state.needsList && this.state.needsList.length > 0 && (
+              <h4>Needs:</h4>
+            )}
+            {this.state.needsList &&
+              this.state.needsList.map((need, index) => {
+                return (
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={`/seniors/${this.state._id}/needs/${need._id}`}
+                  >
+                    <div className="senior-profile-need" key={index}>
+                      {need.title}
+                    </div>
+                  </Link>
+                );
+              })}
+            <div>
+              <AddNeed theSenior={this.state} getSenior={this.getSenior} />{" "}
             </div>
           </div>
-        );
-    }
+        </div>
+
+        <div className="increase-bottom-margin">
+          <Link
+            style={{ textDecoration: "none" }}
+            className="form-button"
+            to={"/seniors"}
+          >
+            Back to Seniors
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default SeniorProfile
+export default SeniorProfile;
